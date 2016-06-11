971dcfccee8d6af22af4699c7afd6fcce72893f7
Pavel Yatsuk <pavely@google.com>
2016-06-10 23:53:28 -0700

    step

--- LineUpRabbits/solution.py
+++ LineUpRabbits/solution.py
@@ -1,7 +1,17 @@
+import itertools
+
 def answer(x, y, n):
-  pass
-
-
+  count = 0
+  permutations_considered = 0
+  for lineup in itertools.permutations(range(n)):
+    permutations_considered += 1
+    if CountRabbitsVisibleFromLeft(lineup) != x:
+      continue
+    if CountRabbitsVisibleFromRight(lineup) != y:
+      continue
+    count += 1
+  print(permutations_considered)
+  return str(count)
 
 def CountRabbitsVisibleFromLeft(lineup):
   tallest = -1
@@ -12,9 +22,18 @@ def CountRabbitsVisibleFromLeft(lineup):
       count += 1
   return count
 
+def CountRabbitsVisibleFromRight(lineup):
+  tallest = -1
+  count = 0
+  for rabbit in reversed(lineup):
+    if rabbit > tallest:
+      tallest = rabbit
+      count += 1
+  return count
 
 def main():
-  print(CountRabbitsVisibleFromLeft([3,2,1]))
+  print(answer(2,2,3))
+  print(answer(1,2,6))
 
 if __name__ == '__main__':
   main()
