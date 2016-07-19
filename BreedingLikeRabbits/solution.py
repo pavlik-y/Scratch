b98e8e9a5ed68babeccf5907f163d7d9d64963b6
Pavel Yatsuk <pavely@google.com>
2016-07-18 18:00:50 -0700

    step

--- BreedingLikeRabbits/solution.py
+++ BreedingLikeRabbits/solution.py
@@ -55,47 +55,103 @@
 # finished editing your code, use submit [file] to submit your answer. If your
 # solution passes the test cases, it will be removed from your home folder.
 
-def SumDigit(a, b, ci):
-  s = ord(a) + ord(b) + ord(ci) - 144
-  r = chr(s%10 + 48)
-  co = chr(s/10 + 48)
-  return (r, co)
+import heapq
 
-def LongSum(a, b):
-  l = max(len(a), len(b))
-  a = a.zfill(l)
-  b = b.zfill(l)
-  c = "0"
-  res = ""
-  for i in xrange(l):
-    (r, c) = SumDigit(a[-1 - i], b[-1 -i], c)
-    res += r
-  if c != "0":
-    res += c
-  return res[::-1]
+class F:
+  def __init__(self):
+    self.cache = {}
+    self.cache[0] = 1
+    self.cache[1] = 1
+    self.cache[2] = 2
 
+# R(2n) = R(n) + R(n + 1) + n (for n > 1)
+# R(2n + 1) = R(n - 1) + R(n) + 1 (for n >= 1)
+  def EnsureValueCached(self, x):
+    if x in self.cache:
+      return
+    toCalculateSet = set([x])
+    toCalculateQueue = []
+    heapq.heappush(toCalculateQueue, x)
+    while len(toCalculateSet) > 0:
+      x = toCalculateQueue[0]
+      n = x / 2
+      if x % 2 == 0:
+        (a, b) = (n, n + 1)
+      else:
+        (a, b) = (n - 1, n)
+      if a in self.cache and b in self.cache:
+        self.cache[x] = self.Calc(x)
+        toCalculateSet.remove(x)
+        heapq.heappop(toCalculateQueue)
+        continue
+      if a not in self.cache and a not in toCalculateSet:
+        toCalculateSet.add(a)
+        heapq.heappush(toCalculateQueue, a)
+      if b not in self.cache and b not in toCalculateSet:
+        toCalculateSet.add(b)
+        heapq.heappush(toCalculateQueue, b)
 
+  def GetCached(self, x):
+    assert(x in self.cache)
+    return self.cache[x]
 
-def AddDeps(deps, n):
-  if n < 0:
-    return
-  if n in deps:
-    return
-  deps.add(n)
-  AddDeps(deps, n/2)
-  if n % 2 == 1:
-    AddDeps(deps, n/2-1)
-  else:
-    AddDeps(deps, n/2+1)
+  def Calc(self, x):
+    n = x / 2
+    if x % 2 == 0:
+      result = self.GetCached(n) + self.GetCached(n + 1) + n
+    else:
+      result = self.GetCached(n - 1) + self.GetCached(n) + 1
+    return result
 
-def PrintDependencies(n):
-  deps = set()
-  AddDeps(deps, n)
-  print(n)
-  print(sorted(deps))
+  def run(self, x):
+    self.EnsureValueCached(x)
+    return self.cache[x]
+
+def odd(x):
+  return 2 * x + 1
+
+def even(x):
+  return 2 * x
+
+def BinarySearch(v, f):
+  i = 0
+  while True:
+    x = 1 << i
+    if f(x) == v:
+      return x
+    if f(x) > v:
+      break
+    i += 1
+  # {PAV} Handle special case
+  i -= 1
+  x = 1 << i
+  while True:
+    i -= 1
+    if i < 0:
+      return None
+    x1 = x + (1 << i)
+    if f(x1) == v:
+      return x1
+    if f(x1) < v:
+      x = x1
+  return None
+
+def answer(x):
+  x = long(x)
+  f = F()
+  e = lambda x: f.run(even(x))
+  o = lambda x: f.run(odd(x))
+  eres = BinarySearch(x, e)
+  ores = BinarySearch(x, o)
+  if eres > ores:
+    return str(2*eres)
+  if ores != None:
+    return str(2*ores+1)
+  return "None"
 
 def main():
-  print(LongSum("923", "456"))
+  print(answer("7"))
+  print(answer("100"))
 
 if __name__ == '__main__':
   main()
