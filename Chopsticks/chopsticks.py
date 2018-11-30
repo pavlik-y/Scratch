eecf3137a161dda61f25ca9c005dee591d33ac35
Pavel Yatsuk <pavely@google.com>
2018-11-30 09:57:33 -0800

    Working chopstick implementation

--- Chopsticks/chopsticks.py
+++ Chopsticks/chopsticks.py
@@ -1,108 +1,135 @@
-import copy
+import collections
 
-class GameState:
-  def __init__(self, state, turn):
-    self.state = state
-    self.turn = turn
-    self.next_states = []
-    self.id = ""
-  def PlayerToStr(self, player):
-    return "%d%d" % (player[0], player[1])
-  def __str__(self):
-    return "%s:%s:%d" % (self.PlayerToStr(self.state[0]), self.PlayerToStr(self.state[1]), self.turn)
-  def __repr__(self):
-    return str(self)
-  def GenerateAttack(self, attack_hand, defend_hand):
-    state = copy.deepcopy(self.state)
-    state[1 - self.turn][defend_hand] += state[self.turn][attack_hand]
-    if state[1 - self.turn][defend_hand] > 4:
-      state[1 - self.turn][defend_hand] = 0
-    state[1 - self.turn].sort()
-    return GameState(state, 1 - self.turn)
-  def GenerateSplits(self):
-    splits = []
-    total = self.state[self.turn][1]
-    for i in range(1, (self.state[self.turn][1] / 2) + 1):
-      state = copy.deepcopy(self.state)
-      state[self.turn][0] = i
-      state[self.turn][1] = total - i
-      splits.append(GameState(state, 1 - self.turn))
-    return splits
+Player = collections.namedtuple('Player', ['l', 'r'])
+State = collections.namedtuple('State', ['p1', 'p2', 'p1_turn'])
+Move = collections.namedtuple('Move', ['fr', 'to'])
 
-  def GeneratePossibleMoves(self):
-    possible_moves = []
-    if self.state[self.turn][0] + self.state[self.turn][1] == 0:
-      return []
-    possible_moves.append(self.GenerateAttack(0, 0))
-    possible_moves.append(self.GenerateAttack(0, 1))
-    possible_moves.append(self.GenerateAttack(1, 0))
-    possible_moves.append(self.GenerateAttack(1, 1))
-    if self.state[self.turn][0] == 0:
-      possible_moves.extend(self.GenerateSplits())
-    return possible_moves
-  def IsFinal(self):
-    if self.state[0][0] + self.state[0][1] == 0:
-      return True
-    if self.state[1][0] + self.state[1][1] == 0:
-      return True
+def IsPlayerValid(pl):
+  if pl.l < 0 or pl.r < 0:
     return False
-  def IsLoosing(self):
-    for ns in self.next_states:
-      if ns.IsFinal():
-        return True
+  if pl.l > 4 or pl.r > 4:
     return False
+  return True
 
+def CanonicalizePlayer(pl):
+  if pl.l > pl.r:
+    pl = Player(l=pl.r, r=pl.l)
+  return pl
+
+def GenerateSplits(pl):
+  result = set()
+  for i in xrange(1, pl.l):
+    new_pl = Player(l=pl.l-i, r=pl.r+i)
+    if not IsPlayerValid(new_pl):
+      continue
+    new_pl = CanonicalizePlayer(new_pl)
+    if new_pl == pl:
+      continue
+    result.add(new_pl)
+  for i in xrange(1, pl.r):
+    new_pl = Player(l=pl.l+i, r=pl.r-i)
+    if not IsPlayerValid(new_pl):
+      continue
+    new_pl = CanonicalizePlayer(new_pl)
+    if new_pl == pl:
+      continue
+    result.add(new_pl)
+  return result
+
+def TrimPlayer(pl):
+  if pl.l > 4:
+    assert(pl.r <= 4)
+    return Player(0, pl.r)
+  if pl.r > 4:
+    assert(pl.l <= 4)
+    return Player(pl.l, 0)
+  return pl
+
+def GenerateAttacks(p1, p2):
+  result = []
+  if p1.l > 0:
+    if p2.l > 0:
+      result.append(Player(p2.l + p1.l, p2.r))
+    if p2.r > 0:
+      result.append(Player(p2.l, p2.r + p1.l))
+  if p1.r > 0:
+    if p2.l > 0:
+      result.append(Player(p2.l + p1.r, p2.r))
+    if p2.r > 0:
+      result.append(Player(p2.l, p2.r + p1.r))
+  return set([CanonicalizePlayer(TrimPlayer(pl)) for pl in result])
+
+def GenerateNextStates(state):
+  if state.p1_turn:
+    for s in  GenerateSplits(state.p1):
+      yield State(s, state.p2, False)
+    for a in GenerateAttacks(state.p1, state.p2):
+      yield State(state.p1, a, False)
+  else:
+    for s in  GenerateSplits(state.p2):
+      yield State(state.p1, s, True)
+    for a in GenerateAttacks(state.p2, state.p1):
+      yield State(a, state.p2, True)
+
+def GenerateGraph(initial_state):
+  visited_states = set()
+  to_explore = [initial_state]
+  graph = {}
+  while len(to_explore) > 0:
+    state = to_explore.pop()
+    graph[state] = set()
+    for next_state in GenerateNextStates(state):
+      graph[state].add(next_state)
+      if next_state in visited_states:
+        continue
+      visited_states.add(next_state)
+      to_explore.append(next_state)
+  return graph
+
+def IsLostState(st):
+  if st.p1_turn and st.p1.l == 0 and st.p1.r == 0:
+    return True
+  return False
+
+def AnalyzeBadStates(graph):
+  bad_states = set()
+  for st in graph:
+    if IsLostState(st):
+      bad_states.add(st)
+  while True:
+    changed = False
+    for st in graph:
+      if st in bad_states:
+        continue
+      if st.p1_turn:
+        if graph[st] & bad_states == graph[st]:
+          changed = True
+          bad_states.add(st)
+      else:
+        if graph[st] & bad_states:
+          changed = True
+          bad_states.add(st)
+    if not changed:
+      break
+  return bad_states
+
+def FindBadMoves(graph, bad_states):
+  for st1 in graph:
+    if st1 in bad_states:
+      continue
+    for st2 in graph[st1] & bad_states:
+      yield Move(st1, st2)
 
 def main():
-  initial_state = GameState(([1,1],[1,1]), 0)
-  state_queue = []
-  state_map = {}
-  state_queue.append(initial_state)
-  state_map[str(initial_state)] = initial_state
-  steps = 0;
-  while len(state_queue) > 0:
-    steps += 1
-    state = state_queue.pop()
-    # print(str(state))
-    next_states = state.GeneratePossibleMoves()
-    for next_state in next_states:
-      if str(next_state) in state_map:
-        state.next_states.append(state_map[str(next_state)])
-        continue
-      state.next_states.append(next_state)
-      state_map[str(next_state)] = next_state
-      state_queue.append(next_state)
-  id = 0
-  for s in state_map:
-    state = state_map[s]
-    id += 1
-    state.id = ("s%d" % id)
-    print('state "%s" as %s' % (s, state.id))
-  for s in state_map:
-    state = state_map[s]
-    for next_state in state.next_states:
-      print("%s -> %s" % (state.id, next_state.id))
+  st = State(Player(1,1), Player(1,1), True)
+  graph = GenerateGraph(st)
+  print(len(graph))
+  bad_states = AnalyzeBadStates(graph)
+  print(len(bad_states))
+  bad_moves = list(FindBadMoves(graph, bad_states))
+  print(len(bad_moves))
+  for move in bad_moves:
+    print("%s -> %s" % (move.fr, move.to))
 
-
-  # print(steps)
-  # loosing_count = 0
-  # for s in state_map:
-  #   state = state_map[s]
-  #   if state.IsLoosing():
-  #     print("%s is loosing" % state)
-  #     loosing_count += 1
-  # print loosing_count
-  # for s in state_map:
-  #   state = state_map[s]
-  #   if state.IsFinal():
-  #     continue
-  #   bad_state = True
-  #   for ns in state.next_states:
-  #     if not ns.IsLoosing():
-  #       bad_state = False
-  #       break
-  #   if bad_state:
-  #     print("%s is bad_state" % state)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
   main()
